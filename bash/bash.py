2317ac765b0f556d987b3c10a0ddd95f4e40c0fa
Haruki N. Grande <22057153+HarukiGrande@users.noreply.github.com>
2018-06-05 13:09:41 +0100

    Create bash.py

---
+++ bash/bash.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import subprocess
+import asyncio
+from subprocess import Popen
+import threading
+from asyncio.subprocess import PIPE
+from redbot.core.utils.chat_formatting import pagify
+
+class Owner:
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(aliases=["shell"])
+    @commands.is_owner()
+    async def bash(self, ctx, *, arg):
+        """Bash shell"""
+        proc = await asyncio.create_subprocess_shell(arg, stdin=None, stderr=None, stdout=PIPE)
+        out = await proc.stdout.read()
+        msg = pagify(out.decode('utf-8'))
+        await ctx.send(f"```ini\n\n[Bash Input]: {arg}\n```")
+        for page in msg:
+            await ctx.send("```py\n\n{}```".format(page))
