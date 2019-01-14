63653bb476d19eba1250535ab6a40c65355523df
Haruki N. Grande <me@harukigrande.com>
2019-01-14 09:43:16 +0000

    Update bash.py

--- bash/bash.py
+++ bash/bash.py
@@ -4,7 +4,7 @@ import subprocess
 import asyncio
 from subprocess import Popen
 import threading
-from asyncio.subprocess import PIPE
+from asyncio.subprocess import PIPE, STDOUT
 from redbot.core.utils.chat_formatting import pagify
 
 BaseCog = getattr(commands, "Cog", object)
@@ -17,7 +17,7 @@ class Bash(BaseCog):
     @checks.is_owner()
     async def bash(self, ctx, *, arg):
         """Bash shell"""
-        proc = await asyncio.create_subprocess_shell(arg, stdin=None, stderr=PIPE, stdout=PIPE)
+        proc = await asyncio.create_subprocess_shell(arg, stdin=None, stderr=STDOUT, stdout=PIPE)
         out = await proc.stdout.read()
         msg = pagify(out.decode('utf-8'))
         await ctx.send(f"```ini\n\n[Bash Input]: {arg}\n```")
