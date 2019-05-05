f8c4aebaf08f72f0b08676cb7c218d9ac9b510c7
Haruki <me@harukigrande.com>
2019-05-05 12:03:45 +0100

    oof

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -3,18 +3,24 @@ from redbot.core import checks, commands
 from redbot.core import Config
 from redbot.core.data_manager import bundled_data_path
 from aiohttp import web
+import asyncio
 
 BaseCog = getattr(commands, "Cog", object)
 
 class WebServer(BaseCog):
     def __init__(self, bot):
         self.bot = bot
-        bot.loop.create_task(self.make_webserver())
+        self.host = "0.0.0.0"
         self.server = None
         self.app = web.Application()
         self.handler = None
         self.config = Config.get_conf(self, identifier=3723456754567, force_registration=True)
         self.config.register_global(port=8000)
+        self.web = self.bot.loop.create_task(self.make_webserver())
+
+    def __unload(self):
+        self.web.cancel()
+        asyncio.gather(runner.cleanup())
 
     @commands.group()
     async def webserver(self, ctx):
@@ -42,11 +48,14 @@ class WebServer(BaseCog):
     async def port(self, ctx, port:int):
         """Change webServer port."""
         await self.config.port.set(port)
-        await ctx.send("Port updated!")
+        await ctx.send("Port updated!\nRestart to use the new port.")
 
     async def make_webserver(self):
+        global runner
+        routes = web.RouteTableDef()
 
-        async def page(request):
+        @routes.get('/')
+        async def index(request):
             try:
                 filepath = bundled_data_path(self) / 'index.html'
                 with open(filepath) as f:
@@ -57,20 +66,12 @@ class WebServer(BaseCog):
                     body = f.read()
             return web.Response(text=body, content_type='text/html')
 
-        try:
-            self.app.router.add_get('/', page)
-            self.handler = self.app.make_handler()
-            port = await self.config.port()
-            self.server = await self.bot.loop.create_server(self.handler, '0.0.0.0', port)
-            print(self.server)
-            print(f"Serving webserver on port {port}")
-        except:
-            pass
-
-    def __unload(self):
-        print(self.server)
-        self.server.close()
-        self.bot.loop.run_until_complete(self.server.wait_closed())
-        self.bot.loop.run_until_complete(self.app.shutdown())
-        self.bot.loop.run_until_complete(self.handler.finish_connections(60.0))
-        self.bot.loop.run_until_complete(self.app.cleanup())
+        await asyncio.sleep(10)
+        app = web.Application()
+        app.add_routes(routes)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        port = await self.config.port()
+        site = web.TCPSite(runner, self.host, port)
+        await site.start()
+        print(f'[WEBSERVER] Serving on http://{self.host}:{port}')
